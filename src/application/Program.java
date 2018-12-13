8a4d0084f92dd7fac3217b0e65aeaea0fe5a15b5
Jean Crus <jean.carlos@pucpr.edu.br>
2018-12-13 16:08:17 -0200

    Camada xadrez e imprimindo tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
-import boardgame.Posicao;
-import boardgame.Tabuleiro;
+import chess.PartidaDeXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
+		UI.imprimirTabuleiro(partidaDeXadrez.getPecas());
+		
 	}
 
 }
